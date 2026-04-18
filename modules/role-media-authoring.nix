dc8947bd1b67ebea6aa62e61b5879cee723b3a01
Tomasz Guzik <github@tguzik.com>
2026-04-18 12:14:09 +0200

    feat(role-media-authoring): Add inactive entry for DaVinci Resolve

--- modules/role-media-authoring.nix
+++ modules/role-media-authoring.nix
@@ -7,5 +7,6 @@
   environment.systemPackages = with pkgs; [
     audacity # Sound editor with graphical UI
     gimp # GNU Image Manipulation Program
+    # davinci-resolve # UNFREE, requires discrete GPU # Professional video editing, color, effects and audio post-processing
   ];
 }
