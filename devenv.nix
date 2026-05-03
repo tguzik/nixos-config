6cdf7010516df5c745d2a91b0f82c7e9ce08d5c7
Tomasz Guzik <github@tguzik.com>
2026-05-03 14:16:42 +0200

    build: Update derivation diffing, sort pre-commit hooks into groups

--- devenv.nix
+++ devenv.nix
@@ -22,18 +22,29 @@
     go-task
     gnupg
 
+    # Secret management
     sops # Secret management, https://github.com/getsops/sops
     age # Secret encryption, https://github.com/FiloSottile/age + https://github.com/Mic92/sops-nix
     ssh-to-age # Create age key based on machine's SSH key https://github.com/Mic92/ssh-to-age
 
-    deadnix # Find and remove dead code in .nix files
-    nvd # Display a diff between Nix profiles/flakes/generations
-    statix # Static analysis of .nix files
+    # Derivation diffing
+    nix-diff # https://hackage.haskell.org/package/nix-diff # Explain why two Nix derivations differ
+    dix # https://github.com/faukah/dix # Blazingly fast tool to diff Nix related things
   ];
 
   git-hooks.hooks = {
-    deadnix.enable = true;
+    # Basics
     gitlint.enable = true;
+    no-commit-to-branch.enable = false;
+    trufflehog.enable = true;
+
+    # Keep nix files nice and tidy
+    deadnix.enable = true;
+    nixfmt.enable = true;
+    shellcheck.enable = true;
+    statix.enable = true;
+
+    # Format non-nix files
     markdownlint = {
       enable = true;
       settings.configuration = {
@@ -44,11 +55,6 @@
         MD034 = false;
       };
     };
-    nixfmt.enable = true;
-    no-commit-to-branch.enable = false;
-    statix.enable = true;
-    shellcheck.enable = true;
-    trufflehog.enable = true;
     yamllint = {
       enable = true;
       settings.configuration = ''
