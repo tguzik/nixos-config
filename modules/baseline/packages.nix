55280a3202ff5e23c6b88c941ebda5203b52b333
Tomasz Guzik <github@tguzik.com>
2026-05-03 14:17:42 +0200

    feat(baseline): Replace nix-diff with nh in the baseline profile

--- modules/baseline/packages.nix
+++ modules/baseline/packages.nix
@@ -8,12 +8,12 @@
       file # Program that shows the type of files
       gitFull # Distributed version control system
       go-task # https://github.com/go-task/task # Task runner / simpler Make alternative written in Go
-      mc # https://github.com/MidnightCommander/mc # File Manager and User Shell for the GNU Project, known as Midnight Commander
       lsof # https://github.com/lsof-org/lsof # LiSt Open Files
-      ncdu # Disk usage analyzer with an ncurses interface
+      mc # https://github.com/MidnightCommander/mc # File Manager and User Shell for the GNU Project, known as Midnight Commander
       nano # Small, user-friendly console text editor
+      ncdu # Disk usage analyzer with an ncurses interface
       netcat-gnu # Utility which reads and writes data across network connections
-      nix-diff # https://hackage.haskell.org/package/nix-diff # Explain why two Nix derivations differ
+      nh # https://github.com/nix-community/nh # Yet another nix cli helper
       ripgrep # https://github.com/BurntSushi/ripgrep # Recursively searches directories for a regex pattern while respecting your gitignore
       tmux # https://github.com/tmux/tmux/wiki # Terminal multiplexer
       vim # Most popular clone of the VI editor
