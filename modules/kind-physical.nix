459eadb8111b5b06658962420209f556fe6b5c23
Tomasz Guzik <github@tguzik.com>
2026-05-03 14:53:29 +0200

    feat(physical): Add lshw package

--- modules/kind-physical.nix
+++ modules/kind-physical.nix
@@ -8,10 +8,11 @@
   ];
 
   environment.systemPackages = with pkgs; [
-    pciutils # lspci; enumerate pci devices
-    usbutils # lsusb; enumerate usb devices
+    acpi # Show battery status and other ACPI information
     dmidecode # read detailed hardware information from bios/uefi
     fastfetch # https://github.com/fastfetch-cli/fastfetch # Feature-rich neofetch like system information tool
-    acpi # Show battery status and other ACPI information
+    lshw # https://ezix.org/project/wiki/HardwareLiSter # Provide detailed information on the hardware configuration of the machine
+    pciutils # lspci; enumerate pci devices
+    usbutils # lsusb; enumerate usb devices
   ];
 }
