019ea84be27772f819910e712adfb4efba4548dd
Tomasz Guzik <github@tguzik.com>
2026-05-03 14:37:07 +0200

    feat(media): Add Inkscape and Krita to the media-authoring role

--- modules/role-media-authoring.nix
+++ modules/role-media-authoring.nix
@@ -7,6 +7,8 @@
   environment.systemPackages = with pkgs; [
     audacity # Sound editor with graphical UI
     gimp # GNU Image Manipulation Program
+    inkscape # Vector graphics editor
+    krita # Free and open source painting application
     # davinci-resolve # UNFREE, requires discrete GPU # Professional video editing, color, effects and audio post-processing
   ];
 }
