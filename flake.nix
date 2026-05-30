6729d42e443260a7960681fd547e3ec38168cd81
Tomasz Guzik <github@tguzik.com>
2026-05-30 20:37:24 +0200

    feat: Upgrade to NixOS 26.05

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "Flake with system configurations.";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.11";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-26.05";
     nixpkgs-unstable.url = "github:NixOS/nixpkgs/nixos-unstable";
     sops-nix = {
       url = "github:Mic92/sops-nix";
